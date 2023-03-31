5f0bcb99786f7d0fbfb2e876c125be6a530a1088
Seung-Ki, Kim <62886544+wlvlrtm@users.noreply.github.com>
2023-03-31 22:32:09 +0900

    ScrollingObject 수정

--- Assets/Scripts/ScrollingObject.cs
+++ Assets/Scripts/ScrollingObject.cs
@@ -5,7 +5,9 @@ public class ScrollingObject : MonoBehaviour {
     public float speed = 10f; // 이동 속도
 
     private void Update() {
-        // 게임 오브젝트를 왼쪽으로 일정 속도로 평행 이동하는 처리
-        transform.Translate(Vector3.left * this.speed * Time.deltaTime);
+        if (!GameManager.instance.isGameover) {
+            // 게임 오브젝트를 왼쪽으로 일정 속도로 평행 이동하는 처리
+            transform.Translate(Vector3.left * this.speed * Time.deltaTime);
+        }
     }
 }
